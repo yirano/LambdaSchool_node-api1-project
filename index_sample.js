e14b91780667676a8c1cb2874af3f0184af73453
yirano <yirano@protonmail.ch>
2020-05-07 22:25:42 -0400

    Commit working get request to /api/users

--- index_sample.js
+++ index_sample.js
@@ -1,67 +1,100 @@
-// require the express npm module, needs to be added to the project using 'yarn add express'
+import express from 'express'
 import users from './data/data.json'
 
-const express = require('express')
-
-// creates an express application using the express module
 const app = express();
-const PORT = 8000
+const PORT = 8000;
 
-app.get('/', (req, res) => {
-  res.send(users)
+// NOTE Express lesson from Lynda.com
+/**
+ *
+app.get('/item/:id', (req, res, next) => {
+  let user = users.filter(user => user.id === Number(req.params.id))
+  res.send(user)
+  next();
+}, (req, res) => {
+  console.log('Did you get the right data?')
 })
 
+app.route('/item')
+  .get((req, res) => {
+    // res.send(`a put request with /item on port ${PORT}`)
+    // res.end() --> ends API call
+    // res.redirect('http://www.linkedIn.com') --> Redirects to new URL
+    // res.download('images/rocket.jpg') --> Automatically starts a download action
+    res.send(`a get request with /item route on port ${PORT}`)
+  })
+  .put((req, res) => {
+    res.send(`a put request with /item on port ${PORT}`)
+  })
+  .delete((req, res) => {
+    res.send(`a delete request with /item on port ${PORT}`)
+  })
 
-// TODO Client makes a POST request /api/users
+app.listen(PORT, () => {
+  console.log(`Your server is running on port ${PORT}`);
+  console.log(users);
+})
+ */
+
+
+
+
+
+/**
+// NOTE Client makes a POST request /api/users
 app.post('/api/users', (req, res) => {
+
   // TODO if the request body is missing the name or bio property:
+  // - respond with HTTP status code 400
+  // - return the following JSON response { errorMessage: "Please provide name and bio for the user." }
   if (req.body.name === undefined || req.body.bio === undefined) {
-    // - respond with HTTP status code 400
-    // - return the following JSON response { errorMessage: "Please provide name and bio for the user." }
     return req.status(400).send("Please provide name and bio for the user")
-  } else if (req.body.name !== undefined && req.body.bio !== undefined) {
+
     // TODO if the information about the user is valid:
     // - respond with HTTP status code 201 (created)
     // - return the newly created user document
+  } else if (req.body.name !== undefined && req.body.bio !== undefined) {
     const newUser = req.body;
     users.push(newUser)
     return res.status(201).json(users)
-  } else {
+
     // TODO if there's an error while saving the user:
     // - respond with HTTP status code 500 (server error)
     // - return the following JSON object: { errorMessage: "There was an error while saving the user to the database" }
-    return res.status(500).send('There was an error while saving the user to the database')
+  } else {
+    return res.status(500).json({ errorMessage: "There was an error while saving the user to the database" })
   }
 })
 
-// REVIEW Client makes a GET request to /api/users
+// NOTE Client makes a GET request to /api/users
 app.get('/api/users', (req, res) => {
   // TODO If there's an error in retrieving the users from the database:
-  if (users.length < 0) {
-    // - respond with HTTP status code 500
-    // - return the following JSON object: { errorMessage: "The users information could not be retrieved." }
-    res.status(500).json({ errorMessage: "The users information could not be retrieved." })
+  // - respond with HTTP status code 500
+  // - return the following JSON object: { errorMessage: "The users information could not be retrieved." }
+  if (!users || users === undefined) {
+    res.status(500).send("The users information could not be retrieved") // REVIEW
   } else {
-    res.send(users)
+    res.json(users)
   }
 })
 
 // NOTE Client makes a GET request to /api/users/:id
 app.get('/api/users/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const user = users.filter(user => user.id === id)
   // TODO if the user with the specified id is not found
-  // const user = users.filter(user => user.id === Number(req.params.id))
-  if (user.length === 0) {
-    // - respond with HTTP status code 404 (not found)
-    // - return the following JSON object: { message: "The user with the specified ID does not exist." }
-    res.status(404).json({ message: "The user with the specified ID does not exist." })
-  } else if (user.length === -1) {
-    // TODO if there's an error in retrieving the user from the database:
-    // - respond with HTTP status code 500
-    // - return the following JSON object: { errorMessage: "The user information could not be retrieved." }
-    res.status(500).send("The user information could not be retrieved.")
+  // - respond with HTTP status code 404 (not found)
+  // - return the following JSON object: { message: "The user with the specified ID does not exist." }
+  if (user.length <= 0) {
+    res.status(400).send("The user with the specified ID does not exist.")
   }
-  res.send(user)
+  // TODO if there's an error in retrieving the user from the database:
+  // - respond with HTTP status code 500
+  // - return the following JSON object: { errorMessage: "The user information could not be retrieved." }
+  else {
 
+    res.json(user)
+  }
 })
 
 // NOTE Client makes a DELETE request to /api/users/:id
@@ -95,5 +128,5 @@ app.put('/api/users/:id', (req, res) => {
   // - return the newly updated user document
 })
 
-
-app.listen(PORT, () => console.log(`API running on port 8000`))
+app.listen(8000, () => console.log(`API running on port 8000`))
+*/
